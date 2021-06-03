bc48a5d52bcf3ac8bc3d02953b7ac9c4da636d80
markstien <markstien@outlook.com>
2021-06-03 20:05:41 +0800

    httpParser

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-console.log("hello!");
+import net from 'net';
+import parser from './src/httpParser';
+
+const PORT = 8080;
+
+const server = net.createServer();
+
+server.on('connection',(socket) => {
+    //创建
+    console.log('客户端：',socket.remoteAddress);
+
+    socket.setEncoding('binary');
+
+    socket.on('data',(data) => {
+        console.log(JSON.stringify(data));
+        const { method, path, headers } = parser(JSON.stringify(data));
+        console.log(headers);
+
+        socket.write(`HTTP/1.1 200 OK
+Content-Type: text/plain
+Content-Length: 5
+
+hello`);
+    });
+
+    socket.on('close',(hadError) => {
+        console.log('socket关闭是否有误:',hadError);
+    })
+
+    socket.on('error',(error) => {
+        console.log('socket-error',error);
+    })
+})
+
+server.on('error',(error) => {
+    console.log("server-error",error);
+})
+
+server.listen(PORT);
+
+server.on('listening', () => {
+    // @ts-ignore
+    const { port } = server.address();
+    console.log(`Fly服务器运行在 http://localhost:${port}`);
+})
---
+++ src/httpParser.ts
@@ -0,0 +1,56 @@
+interface HeaderInterface {
+    header: string,
+    content: string
+}
+
+const headersMap = (httpLines: string[])=>{
+    const headerLines:HeaderInterface[] = httpLines.map( (line:string) => {
+        const [header,...content] = line.split(':');
+        if(header==="Host"||header==="host"){
+            return {
+                header,
+                content:`${content[0]}:${content[1]}`
+            }
+        }
+
+        return { header, content: content.toString() }
+    });
+    const headers = Object();
+    const length = headerLines.length;
+
+    for (let i=0; i<length;i++){
+        const { header, content } = headerLines[i];
+        headers[header] = content;
+    }
+    return headers;
+}
+
+const httpParser = (data:String) => {
+    const [head,...body] =  data.toString().split('\\r\\n\\r\\n');
+    console.log("body",body);
+    const [firstLine,...otherLines] = head.toString().split('\\r\\n');
+    const [method,path,httpVersion] = firstLine.trim().split(' ');
+    const headers = headersMap(otherLines);
+    return { method, path, httpVersion, headers };
+};
+
+export default httpParser
+
+
+/**
+ * Test
+ */
+const httpParserTest = () => {
+    const str = `GET /aa HTTP/1.1
+Host: localhost:8080
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: zh-CN,en-US;q=0.7,en;q=0.3
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Upgrade-Insecure-Requests: 1`;
+    const r = httpParser(str);
+
+    console.log(r);
+}
+//httpParserTest();
