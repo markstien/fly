7a56d483f120c9b573e08367ab72e1993dcca172
markstien <markstien@outlook.com>
2021-06-06 11:30:28 +0800

    refactor Fly: clas

--- src/Fly.ts
+++ src/Fly.ts
@@ -5,50 +5,6 @@ export interface FlyInterface {
     run(port:number,callback:(port:number)=>void):void
 }
 
-export function Fly1():FlyInterface {
-    //默认端口
-    let PORT = 8080;
-
-    const server = net.createServer();
-
-    server.on('connection',(socket) => {
-        socket.setEncoding('binary');
-
-        socket.on('data',(data) => {
-            const { method, path, headers } = parser(JSON.stringify(data));
-
-            socket.write(`HTTP/1.1 200 OK
-Content-Type: text/plain
-Content-Length: 5
-
-hello`);
-        });
-
-        socket.on('close',(hadError) => {
-            console.log('socket关闭是否有误:',hadError);
-        })
-
-        socket.on('error',(error) => {
-            console.log('socket-error',error);
-        })
-    })
-
-    server.on('error',(Error) => {
-        throw Error;
-    })
-
-
-    function run(port:number,callback:(port:number)=>void){
-        PORT = port;
-        server.listen(PORT);
-        server.on('listening', ()=>callback(PORT));
-    }
-
-    return {
-        run
-    }
-}
-
 class Fly implements FlyInterface{
     private  PORT:number = 9090
     private server:net.Server
